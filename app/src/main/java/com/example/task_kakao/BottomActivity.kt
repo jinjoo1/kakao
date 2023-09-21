0bc0b6bfd145ae2af1e9cbeb1acc9a99ae5fc9a0
황진주 <ghkd1007@gmail.com>
2023-09-21 19:37:10 +0900

    [add]bottom추가

---
+++ app/src/main/java/com/example/task_kakao/BottomActivity.kt
@@ -0,0 +1,11 @@
+package com.example.task_kakao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BottomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom)
+    }
+}
