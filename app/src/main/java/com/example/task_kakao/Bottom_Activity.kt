7f88005fb6b390a7bb362b375412140421016782
황진주 <ghkd1007@gmail.com>
2023-09-25 01:46:52 +0900

    [add]리사이클러뷰추가

--- app/src/main/java/com/example/task_kakao/Bottom_Activity.kt
+++ app/src/main/java/com/example/task_kakao/Bottom_Activity.kt
@@ -13,11 +13,13 @@ class Bottom_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //여기 좋아요 같은 마이박스에 추가할 기능 넣어야함.
+
         // 바인딩 객체 초기화
         binding = ActivityBottomBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //버튼에 클릭 리스너설정
+        //버튼에 클릭 리스너설정(하단에 searchButton과 myboxbutton누르면 전환되는 클릭리스너)
         binding.run {
             searchButton.setOnClickListener {
                 setFragment(Search_Fragment())
@@ -27,7 +29,7 @@ class Bottom_Activity : AppCompatActivity() {
             }
 
         }
-
+//앱시작시 서치프래그먼트 기본으로 설정해주기
         setFragment(Search_Fragment())
 
     }
